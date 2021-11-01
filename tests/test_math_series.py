95955d306c6ed4eedb6df7d691b87254f18baac1
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-11-01 19:56:17 +0200

    edit

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,5 +1,9 @@
 from math_series import __version__
 from math_series.series import sum_series
+from math_series.series import lucas
+from math_series.series import fibonacci
+
+
 
 
 def test_version():
@@ -13,5 +17,15 @@ def test_sum_series_fibonacci():
 def test_sum_series_lucas():
     expected=29
     actual=sum_series(7,2,1)
+    assert expected == actual
+
+def test_lucas():
+    expected=29
+    actual=lucas(7)
+    assert expected == actual
+
+def test_fibonacci():
+    expected=13
+    actual=fibonacci(7)
     assert expected == actual
 
