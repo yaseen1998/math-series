cb54d060f68057fa7d2bad9db2966aff15cd70c1
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-11-03 11:30:43 +0200

    last edit

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ v:1.0.0
 creating a functionality for fibonacci series and lucas numbers
 
     start - end : 3:00pm - 7:00pm
+
+running server url : null
+running client url : null
+url PR : [PR](https://github.com/yaseen1998/math-series/pull/1)
--- math_series/series.py
+++ math_series/series.py
@@ -1,18 +1,24 @@
-def fibonacci(n):
-    if n==0:
+
+
+def fibonacci(n): # declare fibonacci function 
+    """ Fibonacci sequence, such that each number is the sum of the two preceding ones,
+    starting from 0 and 1. That is"""
+    if n==0: # first number of fibonacci it is 0
         return 0
-    if n==1:
+    if n==1: # second number of fibonacci it is 1
         return 1
-    else:
+    else: # 
         return fibonacci(n-1)+ fibonacci(n-2)
 
 for i in range(8):
     print(fibonacci(i))
 
-def lucas(n):
-    if n== 0:
+def lucas(n): # declare fibonacci lucas 
+    """ lucas sequence, such that each number is the sum of the two preceding ones,
+    starting from 2 and 1. That is"""
+    if n== 0:# first number of fibonacci it is 2
         return 2
-    if n== 1:
+    if n== 1:# second number of fibonacci it is 1
         return 1
     else:
         return lucas(n-1)+ lucas(n-2)
@@ -25,6 +31,8 @@ for i in range(8):
 # fib1+fib0
 # 1+0+1+1+0=3
 def sum_series(n,i=0,j=1):
+    """ sum_series function it's return a  Fibonacci sequence in default i,j 
+    of you change the default i,j then you change the first two number"""
     if n==0:
         return i
     if n==1:
